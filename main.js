889d131e981d560e0a6819c05fec81a736d86d8d
Luciano <lucco6823@gmail.com>
2021-09-06 15:08:19 +0200

    Terminato

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: "#text",
+        data: {
+            message: "LUCIANO MARCHIONNA"
+        }
+    }
+);
